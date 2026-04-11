f2c0beec57c2d18bfe2ffc191c921b50b4cd8f3b
TridevSharma1 <tridevx8@gmail.com>
2026-04-11 19:26:59 +0530

    HTML And Code of model and view

--- Tridev/First/urls.py
+++ Tridev/First/urls.py
@@ -1,6 +1,7 @@
 ﻿from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from .views import * ## Importing all views from the current app's views.py file
 
 urlpatterns = [
-    
+    path('categorie', category_products, name='category_products'),
 ]
